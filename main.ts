3954e3eded07399f3ed9eace17bc82e5d27a5752
IrmaIsfot <skrappost66@hotmail.com>
2022-04-14 15:58:04 +0200

    Uppdatera main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,67 +1,13 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    while (skickat == 0) {
-        basic.pause(1)
-    }
-    if (receivedNumber == Mitt_val) {
-        basic.showLeds(`
-            . . . . .
-            # # # # #
-            . . . . .
-            # # # # #
-            . . . . .
-            `)
-    } else if (Mitt_val == sten) {
-        if (receivedNumber == sax) {
-            basic.showIcon(IconNames.Happy)
-        } else if (receivedNumber == påse) {
-            basic.showIcon(IconNames.Sad)
-        }
-    } else if (Mitt_val == sax) {
-        if (receivedNumber == 3) {
-            basic.showIcon(IconNames.Happy)
-        } else if (receivedNumber == 1) {
-            basic.showIcon(IconNames.Sad)
-        }
-    } else if (Mitt_val == påse) {
-        if (receivedNumber == 3) {
-            basic.showIcon(IconNames.Happy)
-        } else if (receivedNumber == 1) {
-            basic.showIcon(IconNames.Sad)
-        }
-    }
-    skickat = 0
+    mottaget = 1
+    Motståndarens_val = receivedNumber
 })
 input.onButtonPressed(Button.A, function () {
-    Mitt_val = Mitt_val + 1
-    if (Mitt_val == 4) {
-        Mitt_val = 1
-    }
-    if (Mitt_val == sten) {
-        basic.showLeds(`
-            . . # . .
-            . # # # .
-            # # # # #
-            . # # # .
-            . . # . .
-            `)
-    }
-    if (Mitt_val == sax) {
-        basic.showLeds(`
-            # . . . .
-            . # # . .
-            . . . # #
-            . # # . .
-            # . . . .
-            `)
-    }
-    if (Mitt_val == påse) {
-        basic.showLeds(`
-            # . . . #
-            # . . . #
-            # . . . #
-            # . . . #
-            . # # # .
-            `)
+    if (skickat == 0) {
+        Mitt_val = Mitt_val + 1
+        if (Mitt_val == 4) {
+            Mitt_val = 1
+        }
     }
 })
 input.onButtonPressed(Button.B, function () {
@@ -70,16 +16,18 @@ input.onButtonPressed(Button.B, function () {
     skickat = 1
     basic.clearScreen()
 })
+let mottaget = 0
 let skickat = 0
-let påse = 0
-let sax = 0
-let sten = 0
+let Motståndarens_val = 0
 let Mitt_val = 0
 radio.setGroup(1)
 Mitt_val = 1
-sten = 1
-sax = 2
-påse = 3
+Motståndarens_val = 0
+let sten = 1
+let sax = 2
+let påse = 3
+skickat = 0
+mottaget = 0
 basic.showLeds(`
     . . # . .
     . # # # .
@@ -88,5 +36,71 @@ basic.showLeds(`
     . . # . .
     `)
 basic.forever(function () {
-	
+    if (skickat == 0) {
+        if (Mitt_val == sten) {
+            basic.showLeds(`
+                . . # . .
+                . # # # .
+                # # # # #
+                . # # # .
+                . . # . .
+                `)
+        }
+        if (Mitt_val == sax) {
+            basic.showLeds(`
+                # . . . .
+                . # # . .
+                . . . # #
+                . # # . .
+                # . . . .
+                `)
+        }
+        if (Mitt_val == påse) {
+            basic.showLeds(`
+                # . . . #
+                # . . . #
+                # . . . #
+                # . . . #
+                . # # # .
+                `)
+        }
+    }
+    basic.pause(1)
+    if (mottaget == 1) {
+        if (Motståndarens_val == Mitt_val) {
+            basic.showLeds(`
+                . . . . .
+                # # # # #
+                . . . . .
+                # # # # #
+                . . . . .
+                `)
+        } else if (Mitt_val == sten) {
+            if (Motståndarens_val == sax) {
+                basic.showIcon(IconNames.Happy)
+                soundExpression.giggle.playUntilDone()
+            } else if (Motståndarens_val == påse) {
+                basic.showIcon(IconNames.Sad)
+                soundExpression.sad.playUntilDone()
+            }
+        } else if (Mitt_val == sax) {
+            if (Motståndarens_val == 3) {
+                basic.showIcon(IconNames.Happy)
+                soundExpression.giggle.playUntilDone()
+            } else if (Motståndarens_val == 1) {
+                basic.showIcon(IconNames.Sad)
+                soundExpression.sad.playUntilDone()
+            }
+        } else if (Mitt_val == påse) {
+            if (Motståndarens_val == 3) {
+                basic.showIcon(IconNames.Happy)
+                soundExpression.giggle.playUntilDone()
+            } else if (Motståndarens_val == 1) {
+                basic.showIcon(IconNames.Sad)
+                soundExpression.sad.playUntilDone()
+            }
+        }
+        mottaget = 0
+        skickat = 0
+    }
 })
